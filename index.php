0e265748feabc09e11b9ba75d331121de4639826
4infa <sardella.marcello@studenti.salesianisesto.it>
2016-03-14 13:28:26 +0100

    ho aggiunto la riga 75

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+
+    $var = "stringa";
+    $var = 10;
+    $array = [55,2,44];
+
+    $stringa2 = $var."qwerty";
+
+
+//    if($var===10.0){
+//
+//    }
+//    else
+//        echo "no";
+//
+//    for($i=0;$i<10;$i++){
+//        echo "<br>riga".$i;
+//    }
+
+    foreach($array as $riga){
+        echo $chiave." ".$riga."<br />";
+    }
+    echo $array[2];
+
+    function somma($valore1,$valore2){
+        return $valore1+$valore2;
+    }
+    echo somma(10,20);
+
+    class animale{        /**
+         * @return string
+         */
+        public function getNome()
+        {
+            return strtoupper($this->nome);
+        }
+
+        /**
+         * @param string $nome
+         */
+        public function setNome($nome)
+        {
+            $this->nome = $nome;
+        }
+        protected $nome = "";
+
+        /**
+         * @return mixed
+         */
+        public function getSpecie()
+        {
+            return $this->specie;
+        }
+
+        /**
+         * @param mixed $specie
+         */
+        public function setSpecie($specie)
+        {
+            $this->specie = $specie;
+        }
+        public $specie;
+
+        public function presentazione(){
+            return "io sono ".$this->getNome()." della razza ".$this->getSpecie();
+        }
+    }
+
+
+    $cane = new Animale();
+    $cane->setNome("pippo");
+    $cane->setSpecie("cane");
+
+    echo $cane->presentazione();
+    echo $cane->presentazione();
